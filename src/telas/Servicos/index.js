ad3123a692faba80800018a98080c59463f5c53f
Carlos <quintino.carlos@aluno.ufabc.edu.br>
2022-03-20 19:58:46 -0300

    Commit inicial

---
+++ src/telas/Servicos/index.js
@@ -0,0 +1,14 @@
+import react from "react";
+import { Text, SafeAreaView, StatusBar } from "react-native";
+const servicos = [
+    {
+    id: 1,
+    nome: "Banho",
+    preco:79.90,
+    descricao: "Banho de cachorro",
+}
+]
+export default function Servicos(){
+    return <Text> Serviços! </Text>
+            
+}
